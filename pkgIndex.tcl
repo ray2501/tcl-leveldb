035d52a89ba6f715c84f8ecfd5b54506026f0083
Danilo Chang <ray2501@gmail.com>
2017-08-23 19:26:00 +0800

    Bump version to v0.1.2

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded leveldb 0.1.1 \
-    [list load [file join $dir libleveldb0.1.1.so] leveldb]
+package ifneeded leveldb 0.1.2 \
+    [list load [file join $dir libleveldb0.1.2.so] leveldb]
