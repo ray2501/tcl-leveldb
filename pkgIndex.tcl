15c46b675a15bdf39cdb57ac148212beea2438b5
Danilo Chang <ray2501@gmail.com>
2023-12-15 13:07:07 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded leveldb 0.2.1 \
-    [list load [file join $dir libleveldb0.2.1.so] leveldb]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded leveldb 0.2.1 \
+	    [list load [file join $dir libtcl9leveldb0.2.1.so] [string totitle leveldb]]
+} else {
+    package ifneeded leveldb 0.2.1 \
+	    [list load [file join $dir libleveldb0.2.1.so] [string totitle leveldb]]
+}
